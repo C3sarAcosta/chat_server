5ff34a28a8850d50a97e345cdfc46e5766ab793d
César Acosta <c3sar.adan@gmail.com>
2021-11-02 19:09:11 -0700

    Conectar base de datos

--- database/config.js
+++ database/config.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose')
 
 const dbConeccion = async () => {
     try {
-        console.log('init db');
+        await mongoose.connect(process.env.DB_CNN, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('DB Online');
     } catch (error) {
         console.log(error);
         throw new Error('Error en la base de datos');
