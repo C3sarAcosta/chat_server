bbb726c483525e7b638190e400bb69c382193cb5
César Acosta <c3sar.adan@gmail.com>
2021-11-02 20:35:08 -0700

    validando datos crear usuario

--- routes/auth.js
+++ routes/auth.js
@@ -2,11 +2,15 @@
     path: api/login
  */
 const { Router, response } = require('express');
+const { check } = require('express-validator');
 const { crearUsuario } = require('../controllers/auth');
 
 const router = Router();
 
 //End point para crear un usuario
-router.post('/new', crearUsuario)
+router.post('/new', [
+    //Eexpress-validator para validar campos
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+], crearUsuario)
 
 module.exports = router;
