f9f0a98f1b6440dbf8c3f69176fb155077f7fbe5
César Acosta <c3sar.adan@gmail.com>
2021-11-02 20:44:45 -0700

    Creacion de un middleware para la validacion

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@
 const { Router, response } = require('express');
 const { check } = require('express-validator');
 const { crearUsuario } = require('../controllers/auth');
+const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
 
@@ -11,6 +12,9 @@ const router = Router();
 router.post('/new', [
     //Eexpress-validator para validar campos
     check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('password', 'La contraseña es obligatoria').not().isEmpty(),
+    check('email', 'El email es obligatorio').isEmail(),
+    validarCampos
 ], crearUsuario)
 
 module.exports = router;
