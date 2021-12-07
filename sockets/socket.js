29a1b45f334562c05c9aa896b9cf099ce0dac823
César Acosta <c3sar.adan@gmail.com>
2021-12-06 22:35:57 -0700

    Cambiar estado BD

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,19 +1,21 @@
 const { comprobarJWT } = require('../helpers/jwt');
 const { io } = require('../index');
-
+const { usuarioConectado, usuarioDesconectado } = require('../controllers/socket');
 
 // Mensajes de Sockets
-io.on('connection', client => {
+io.on('connection', (client) => {
     console.log('Cliente conectado');
     const [valido, uid] = comprobarJWT(client.handshake.headers['x-token'])
     //console.log(valido, uid);
     if (!valido) {
         return client.disconnect();
     }
-    console.log('cliente autenticado');
+
+    //console.log('cliente autenticado');
+    usuarioConectado(uid);
 
     client.on('disconnect', () => {
-        console.log('Cliente desconectado');
+        usuarioDesconectado(uid);
     });
 
     /*client.on('mensaje', ( payload ) => {
